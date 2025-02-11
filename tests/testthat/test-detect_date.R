a1d7a17c6355294d8d8e9975da00764702f33b80
Gota Mac <Yajima071224@gmail.com>
2025-02-11 17:58:55 +0900

    add new image and rename

--- tests/testthat/test-detect_date.R
+++ tests/testthat/test-detect_date.R
@@ -1,5 +1,5 @@
 testthat::test_that("OCR date loading", {
-  image_path <- system.file("extdata", "cliped_image.jpg", package = "timestampr")
+  image_path <- system.file("extdata", "01-cropped-image.jpg", package = "timestampr")
   image      <- magick::image_read(image_path)
   out        <- detect_date(image)
   expect_equal(lubridate::mdy_hm("06/22/2018 05:57PM"), out)
