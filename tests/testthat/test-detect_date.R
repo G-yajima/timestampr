190dbc046c8dc042fc66f23dfedff1f1df4c65fd
Gota Mac <Yajima071224@gmail.com>
2025-02-11 17:42:29 +0900

    commit: Convert OCR text to datetime passed

--- tests/testthat/test-detect_date.R
+++ tests/testthat/test-detect_date.R
@@ -4,3 +4,11 @@ testthat::test_that("OCR text loading", {
   text       <- detect_date(image)
   expect_match("06/22/2018 05:57PM", text)
 })
+
+testthat::test_that("Convert OCR text to datetime", {
+  text_PM <- "06/22/2018 04:00PM"
+  text_AM <- "06/22/2018 04:00AM"
+
+  expect_equal(lubridate::mdy_hm("06/22/2018 16:00"), lubridate::mdy_hm(text_PM))
+  expect_equal(lubridate::mdy_hm("06/22/2018 04:00"), lubridate::mdy_hm(text_AM))
+})
